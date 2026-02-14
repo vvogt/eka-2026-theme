490201265557fcfa139fe276fe66d01143788f20
Vahur Vogt <43129530+vvogt@users.noreply.github.com>
2026-02-14 10:53:28 +0200

    Started working front-page top section

---
+++ front-page.php
@@ -0,0 +1,39 @@
+<?php
+
+get_header();
+
+?>
+
+<main>
+
+    <section class="top-section">
+
+        <div class="content-container">
+            
+            <div class="top-section__text-content">
+
+                <h1>Õpeta lastele rahatarkust läbi mängu</h1>
+
+                <p>
+                    Rahamäng on lõbus ja hariv lasteraamat, mis õpetab väikestele rahatarkust, säästmist ja targat rahakasutust läbi põnevate lugude ja mängude.
+                </p>
+
+                <a href="" class="btn btn-primary">
+                    Osta raamat
+                </a>
+
+                <a href="" class="btn btn-secondary">
+                    Loe rohkem
+                </a>
+
+            </div>
+
+            <img class="top-section__image" src="<?php echo get_template_directory_uri(); ?>/assets/creatures.png" alt="">
+
+        </div>
+
+    </section>
+
+</main>
+
+<?php get_footer(); ?>
